cb3dee2a33689b6e5f080b58252dcabb2b34dccc
gaozhengxin <44023221+gaozhengxin@users.noreply.github.com>
2022-03-29 02:15:07 +0800

    Update Any721.sol

--- contracts/Any721.sol
+++ contracts/Any721.sol
@@ -1,7 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
+import "github.com/OpenZeppelin/openzeppelin-contracts/contracts/utils/Context.sol";
+
+interface IERC721Mintburnable is IERC721 {
+    function mint(address to, uint256 tokenId) external virtual;
+
+    function burn(uint256 tokenId) external virtual;
+}
 
 abstract contract ERC721Receiver {
     function onERC721Received(
@@ -22,8 +30,14 @@ abstract contract AnyCallProxy {
 
     Context public context;
 
-    function anyCall(address _to, bytes calldata _data, address _fallback, uint256 _toChainID) virtual external;
-    function withdraw(uint256 _amount) virtual external;
+    function anyCall(
+        address _to,
+        bytes calldata _data,
+        address _fallback,
+        uint256 _toChainID
+    ) external virtual;
+
+    function withdraw(uint256 _amount) external virtual;
 }
 
 abstract contract AnyCallClient is Context {
@@ -32,10 +46,21 @@ abstract contract AnyCallClient is Context {
 
     /// key is destination chain id
     /// value is Multichain721 contract on destination chain
-    mapping (uint256 => address) public counterparts;
+    mapping(uint256 => address) public counterparts;
 
     modifier onlyAnyCall() {
         require(_msgSender() == anyCallProxy, "not authorized");
+        (address from, uint256 fromChainId) = AnyCallProxy(anyCallProxy)
+            .context();
+        require(counterparts[fromChainId] == from, "caller is not allowed");
+        _;
+    }
+
+    modifier onlyAnyCallFallback() {
+        require(_msgSender() == anyCallProxy, "not authorized");
+        (address from, uint256 fromChainId) = AnyCallProxy(anyCallProxy)
+            .context();
+        require(address(this) == from, "caller is not allowed");
         _;
     }
 
@@ -49,7 +74,10 @@ abstract contract AnyCallClient is Context {
         anyCallProxy = anyCallProxy_;
     }
 
-    function setAnyCallProxy(address anyCallProxy_) public onlyAnyCallClientAdmin {
+    function setAnyCallProxy(address anyCallProxy_)
+        public
+        onlyAnyCallClientAdmin
+    {
         anyCallProxy = anyCallProxy_;
     }
 
@@ -57,101 +85,203 @@ abstract contract AnyCallClient is Context {
         anyCallClientAdmin = admin_;
     }
 
-    function setCounterpart(address counterpart, uint256 chainId) public onlyAnyCallClientAdmin {
+    function setCounterpart(address counterpart, uint256 chainId)
+        public
+        onlyAnyCallClientAdmin
+    {
         counterparts[chainId] = counterpart;
     }
 
-    function anyCallWithdraw(uint256 amount) public onlyAnyCallClientAdmin returns (bool, bytes memory) {
+    function anyCallWithdraw(uint256 amount)
+        public
+        onlyAnyCallClientAdmin
+        returns (bool, bytes memory)
+    {
         AnyCallProxy(anyCallProxy).withdraw(amount);
         return anyCallClientAdmin.call{value: amount}("");
     }
+
+    function anyFallback(address to, bytes calldata data) external virtual;
 }
 
-contract Any721 is ERC721Enumerable, ERC721Receiver, AnyCallClient {
-  address public underlying;
+abstract contract Any721Router is AnyCallClient {
+    event LogOutbound(
+        address token,
+        uint256 tokenId,
+        address receiver,
+        uint256 toChainId
+    );
+    event LogInbound(
+        address token,
+        uint256 tokenId,
+        address receiver,
+        uint256 fromChainId
+    );
+    event LogOutboundFail(address token, uint256 tokenId);
 
-  event LogDeposit(address user, uint tokenId);
-  event LogWithdraw(address user, uint tokenId);
-  event LogOutbound(uint tokenId, address receiver, uint toChainId);
-  event LogInbound(uint tokenId, address receiver, uint fromChainId);
-  event LogOutboundFail(uint256 tokenId);
+    mapping(address => mapping(uint256 => address)) public tokenMap;
+    address public routerAdmin;
 
-  constructor (string memory name_, string memory symbol_, address underlying_, address anyCallProxy_) ERC721(name_, symbol_) AnyCallClient(anyCallProxy_) {
-    underlying = underlying_;
-  }
+    mapping(address => uint256) public fee;
 
-  /// @notice Get extra data bind to tokenId.
-  function getExtraData(uint tokenId) external pure returns(bytes memory packedData) {
-    /// @dev pack extra data into string
-    packedData = "any extra data";
-    return packedData;
-  }
-
-  /// @notice Set extra data.
-  function setExtraData(uint tokenId, bytes memory extraData) internal {
-    if (underlying == address(0)) {
-      /// @dev unpack extra data and bind it with tokenId
-    } else {
-      /// @dev unpack extra data and bind it with tokenId
+    modifier charge(address token) {
+        require(msg.value >= fee[token]);
+        _;
     }
-    return;
-  }
 
-  /// @notice Deposit underlying 721 nft to this contract and get any721 nft.
-  /// @param tokenId Underlying nft id, which is also any721 nft id.
-  function deposit(uint tokenId) external {
-    IERC721(underlying).safeTransferFrom(msg.sender, address(this), tokenId);
-    _safeMint(msg.sender, tokenId);
-    emit LogDeposit(msg.sender, tokenId);
-  }
-
-  /// @notice Withdraw underlying 721 nft and burn the any721 nft.
-  /// @param tokenId Underlying nft id, which is also any721 nft id.
-  function withdraw(uint tokenId) external {
-    require(msg.sender == ownerOf(tokenId));
-    _burn(tokenId);
-    IERC721(underlying).safeTransferFrom(address(this), msg.sender, tokenId);
-    emit LogWithdraw(msg.sender, tokenId);
-  }
-
-  /// @notice Lock in any721 nft or burn underlying nft, emit an outbound log.
-  function outbound(uint tokenId, address receiver, uint toChainId) external {
-    if (underlying == address(0)) {
-      require(msg.sender == ownerOf(tokenId));
-      _burn(tokenId);
-    } else {
-      require(!_exists(tokenId));
-      IERC721(underlying).safeTransferFrom(msg.sender, address(this), tokenId);
+    constructor(address routerAdmin_, address anyCallProxy_)
+        AnyCallClient(anyCallProxy_)
+    {
+        routerAdmin = routerAdmin_;
     }
-    bytes memory extraData = this.getExtraData(tokenId);
-    bytes memory inboundMsg = abi.encodeWithSignature("inbound(uint256,address,address,bytes)", tokenId, msg.sender, receiver, extraData);
-    AnyCallProxy(anyCallProxy).anyCall(counterparts[toChainId], inboundMsg, address(this), toChainId);
-    emit LogOutbound(tokenId, receiver, toChainId);
-  }
 
-  /// @notice Call by anycall when there's an outbound log. Unlock underlying nft or mint any721 nft to receiver address.
-  function inbound(uint tokenId, address from, address receiver, bytes memory extraData) onlyAnyCall external {
-    if (underlying == address(0)) {
-      _safeMint(msg.sender, tokenId);
-    } else {
-      require(!_exists(tokenId));
-      IERC721(underlying).safeTransferFrom(address(this), receiver, tokenId);
+    function setTokenMap(
+        address originToken,
+        uint256 toChainId,
+        address targetToken
+    ) public {
+        require(msg.sender == routerAdmin);
+        tokenMap[originToken][toChainId] = targetToken;
     }
-    setExtraData(tokenId, extraData);
-    (, uint256 fromChainId) = AnyCallProxy(anyCallProxy).context();
-    emit LogInbound(tokenId, receiver, fromChainId);
-  }
 
-  /// @notice Called by anycall when outbound fails. Return nft to its original owner.
-  function anyFallback(address to, bytes calldata data) external onlyAnyCall {
-    (uint256 tokenId, address from, , bytes memory extraData) = abi.decode(data[4:], (uint256, address, address, bytes));
-    if (underlying == address(0)) {
-      _safeMint(msg.sender, tokenId);
-    } else {
-      require(!_exists(tokenId));
-      IERC721(underlying).safeTransferFrom(address(this), from, tokenId);
+    function setFee(address token, uint256 fee_) public {
+        require(msg.sender == routerAdmin);
+        fee[token] = fee_;
     }
-    setExtraData(tokenId, extraData);
-    emit LogOutboundFail(tokenId);
-  }
+
+    function withdrawFee(address to, uint256 amount) public {
+        require(msg.sender == routerAdmin);
+        (bool success, ) = to.call{value: amount}("");
+        require(success);
+    }
+
+    function outbound(
+        address token,
+        uint256 tokenId,
+        address receiver,
+        uint256 toChainId
+    ) external virtual;
+
+    function inbound(
+        address token,
+        uint256 tokenId,
+        address from,
+        address receiver
+    ) external virtual;
+}
+
+contract Any721MintBurnRouter is Any721Router {
+    constructor(address routerAdmin_, address anyCallProxy_)
+        Any721Router(routerAdmin_, anyCallProxy_)
+    {}
+
+    /// @notice Lock in any721 nft or burn underlying nft, emit an outbound log.
+    function outbound(
+        address token,
+        uint256 tokenId,
+        address receiver,
+        uint256 toChainId
+    ) external override {
+        IERC721Mintburnable(token).burn(tokenId); // require approval
+        bytes memory inboundMsg = abi.encodeWithSignature(
+            "inbound(address,uint256,address,address)",
+            tokenMap[token][toChainId],
+            tokenId,
+            msg.sender,
+            receiver
+        );
+        AnyCallProxy(anyCallProxy).anyCall(
+            counterparts[toChainId],
+            inboundMsg,
+            address(this),
+            toChainId
+        );
+        emit LogOutbound(token, tokenId, receiver, toChainId);
+    }
+
+    /// @notice Call by anycall when there's an outbound log. Unlock underlying nft or mint any721 nft to receiver address.
+    function inbound(
+        address token,
+        uint256 tokenId,
+        address from,
+        address receiver
+    ) external override onlyAnyCall {
+        IERC721Mintburnable(token).mint(receiver, tokenId);
+        (, uint256 fromChainId) = AnyCallProxy(anyCallProxy).context();
+        emit LogInbound(token, tokenId, receiver, fromChainId);
+    }
+
+    /// @notice Called by anycall when outbound fails. Return nft to its original owner.
+    function anyFallback(address to, bytes calldata data)
+        external
+        override
+        onlyAnyCallFallback
+    {
+        (address token, uint256 tokenId, address from, ) = abi.decode(
+            data[4:],
+            (address, uint256, address, address)
+        );
+        IERC721Mintburnable(token).mint(from, tokenId);
+        emit LogOutboundFail(token, tokenId);
+    }
+}
+
+contract Any721PremintRouter is ERC721Receiver, Any721Router {
+    /**
+    The ERC721 token operator should premint tokenIds and transfer them to this contract
+    in order to let users bridge in.
+     */
+    constructor(address routerAdmin_, address anyCallProxy_)
+        Any721Router(routerAdmin_, anyCallProxy_)
+    {}
+
+    /// @notice Lock in any721 nft or burn underlying nft, emit an outbound log.
+    function outbound(
+        address token,
+        uint256 tokenId,
+        address receiver,
+        uint256 toChainId
+    ) external override {
+        IERC721(token).safeTransferFrom(msg.sender, address(this), tokenId);
+        bytes memory inboundMsg = abi.encodeWithSignature(
+            "inbound(address,uint256,address,address)",
+            tokenMap[token][toChainId],
+            tokenId,
+            msg.sender,
+            receiver
+        );
+        AnyCallProxy(anyCallProxy).anyCall(
+            counterparts[toChainId],
+            inboundMsg,
+            address(this),
+            toChainId
+        );
+        emit LogOutbound(token, tokenId, receiver, toChainId);
+    }
+
+    /// @notice Call by anycall when there's an outbound log. Unlock underlying nft or mint any721 nft to receiver address.
+    function inbound(
+        address token,
+        uint256 tokenId,
+        address from,
+        address receiver
+    ) external override onlyAnyCall {
+        IERC721(token).safeTransferFrom(address(this), receiver, tokenId); // asserting tokenId has been premint to this contract
+        (, uint256 fromChainId) = AnyCallProxy(anyCallProxy).context();
+        emit LogInbound(token, tokenId, receiver, fromChainId);
+    }
+
+    /// @notice Called by anycall when outbound fails. Return nft to its original owner.
+    function anyFallback(address to, bytes calldata data)
+        external
+        override
+        onlyAnyCallFallback
+    {
+        (address token, uint256 tokenId, address from, ) = abi.decode(
+            data[4:],
+            (address, uint256, address, address)
+        );
+        IERC721(token).safeTransferFrom(address(this), from, tokenId);
+        emit LogOutboundFail(token, tokenId);
+    }
 }
