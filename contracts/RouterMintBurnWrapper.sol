dba5724af25c399ddaa1e5e4a128841093b2f30d
jowenshaw <jowenshaw@gmail.com>
2022-08-12 10:20:56 +0800

    update RouterMintBurnWrapper with base RoleControl

--- contracts/RouterMintBurnWrapper.sol
+++ contracts/RouterMintBurnWrapper.sol
@@ -8,6 +8,7 @@ pragma solidity ^0.8.10;
 // Notice: the parameters and return type should be same
 interface ITokenMintBurn {
     function mint(address to, uint256 amount) external returns (bool);
+
     function burnFrom(address from, uint256 amount) external returns (bool);
 }
 
@@ -15,33 +16,15 @@ interface ITokenMintBurn {
 // Notice: the parameters and return type should be same
 interface IRouterMintBurn {
     function mint(address to, uint256 amount) external returns (bool);
+
     function burn(address from, uint256 amount) external returns (bool);
 }
 
-// TokenType token type enumerations
-// When in `need approve` situations, the user should approve to this wrapper contract,
-// not to the Router contract, and not to the target token to be wrapped.
-// If not, this wrapper will fail its function.
-enum TokenType {
-    MintBurnAny,  // mint and burn(address from, uint256 amount), don't need approve
-    MintBurnFrom, // mint and burnFrom(address from, uint256 amount), need approve
-    MintBurnSelf, // mint and burn(uint256 amount), call transferFrom first, need approve
-    Transfer,     // transfer and transferFrom, need approve
-    TransferDeposit, // transfer and transferFrom, deposit and withdraw, need approve, block when lack of liquidity
-    TransferDeposit2 // transfer and transferFrom, deposit and withdraw, need approve, don't block when lack of liquidity
-}
-
-// RouterMintBurnWrapper is a wrapper for token that supports `ITokenMintBurn` to support `IRouterMintBurn`
-contract RouterMintBurnWrapper is IRouterMintBurn {
-    // the target token to be wrapped, must support `ITokenMintBurn`
-    address public immutable token;
-    TokenType public constant tokenType = TokenType.MintBurnFrom;
-
-    // set of minters, can be this bridge or other bridges
+// RoleControl has a `vault` (the primary controller)
+// and a set of `minters` (can be this bridge or other bridges)
+abstract contract RoleControl {
     mapping(address => bool) public isMinter;
     address[] public minters;
-
-    // primary controller of this contract
     address public vault;
 
     modifier onlyAuth() {
@@ -54,12 +37,15 @@ contract RouterMintBurnWrapper is IRouterMintBurn {
         _;
     }
 
-    event LogChangeVault(address indexed oldVault, address indexed newVault, uint indexed effectiveTime);
+    event LogChangeVault(
+        address indexed oldVault,
+        address indexed newVault,
+        uint256 indexed effectiveTime
+    );
+    event AddMinter(address _minter);
+    event RevokeMinter(address _minter);
 
-    constructor(address _token, address _vault) {
-        require(_token != address(0) && _token != address(this), "zero token address");
-        token = _token;
-
+    constructor(address _vault) {
         require(_vault != address(0), "zero vault address");
         vault = _vault;
     }
@@ -76,15 +62,45 @@ contract RouterMintBurnWrapper is IRouterMintBurn {
         require(!isMinter[_minter], "minter exists");
         isMinter[_minter] = true;
         minters.push(_minter);
+        emit AddMinter(_minter);
     }
 
     function revokeMinter(address _minter) external onlyVault {
         require(isMinter[_minter], "minter not exists");
         isMinter[_minter] = false;
+        emit RevokeMinter(_minter);
     }
 
     function getAllMinters() external view returns (address[] memory) {
         return minters;
+    }
+}
+
+// TokenType token type enumerations
+// When in `need approve` situations, the user should approve to this wrapper contract,
+// not to the Router contract, and not to the target token to be wrapped.
+// If not, this wrapper will fail its function.
+enum TokenType {
+    MintBurnAny, // mint and burn(address from, uint256 amount), don't need approve
+    MintBurnFrom, // mint and burnFrom(address from, uint256 amount), need approve
+    MintBurnSelf, // mint and burn(uint256 amount), call transferFrom first, need approve
+    Transfer, // transfer and transferFrom, need approve
+    TransferDeposit, // transfer and transferFrom, deposit and withdraw, need approve, block when lack of liquidity
+    TransferDeposit2 // transfer and transferFrom, deposit and withdraw, need approve, don't block when lack of liquidity
+}
+
+// RouterMintBurnWrapper is a wrapper for token that supports `ITokenMintBurn` to support `IRouterMintBurn`
+contract RouterMintBurnWrapper is IRouterMintBurn, RoleControl {
+    // the target token to be wrapped, must support `ITokenMintBurn`
+    address public immutable token;
+    TokenType public constant tokenType = TokenType.MintBurnFrom;
+
+    constructor(address _token, address _vault) RoleControl(_vault) {
+        require(
+            _token != address(0) && _token != address(this),
+            "zero token address"
+        );
+        token = _token;
     }
 
     function mint(address to, uint256 amount) external onlyAuth returns (bool) {
@@ -92,7 +108,11 @@ contract RouterMintBurnWrapper is IRouterMintBurn {
         return true;
     }
 
-    function burn(address from, uint256 amount) external onlyAuth returns (bool) {
+    function burn(address from, uint256 amount)
+        external
+        onlyAuth
+        returns (bool)
+    {
         assert(ITokenMintBurn(token).burnFrom(from, amount));
         return true;
     }
